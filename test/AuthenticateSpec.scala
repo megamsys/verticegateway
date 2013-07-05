4900b5f55c00c38962c61683ddcb86515cccb86f
subashsethurajan <subash.avc@gmail.com>
2013-07-05 20:36:18 +0530

    Changes made in AuthenticateSpec

--- test/AuthenticateSpec.scala
+++ test/AuthenticateSpec.scala
@@ -29,6 +29,7 @@ import org.specs2.execute.{ Result => SpecsResult }
 import com.stackmob.newman.response.{ HttpResponse, HttpResponseCode }
 import com.stackmob.newman._
 import com.stackmob.newman.dsl._
+import controllers.stack.SecurityActions._
 
 /**
  * @author rajthilak
@@ -42,9 +43,9 @@ class AuthenticateSpec extends Specification {
   AuthenticateSpec is the implementation that calls the megam_play API server with the /auth url
   """ ^ end ^
       "The Client Should" ^
-      "Correctly do POST requests with a valid userid and api key" ! Post.succeeds ^
-      // "Correctly do POST requests with a invalid userid and api key" ! PostWithInvalidUserIDEmail.succeeds ^
-      // "Correctly do POST requests with an malformed header" ! PostMalformedHeader.succeeds ^
+     // "Correctly do POST requests with a valid userid and api key" ! Post.succeeds ^
+      "Correctly do POST requests with a invalid userid and api key" ! PostWithInvalidUserIDEmail.succeeds ^
+      //   "Correctly do POST requests with an malformed header" ! PostMalformedHeader.succeeds ^
       end
   /**
    * Change the body content in method bodyToStick
@@ -52,7 +53,7 @@ class AuthenticateSpec extends Specification {
   case object Post extends Context {
 
     protected override def urlSuffix: String = "auth"
-    protected override def bodyToStick: Option[String] = Some(new String("{\"email\":\"sandy@megamsandbox.com\", \"api_key\":\"IamAtlas{74}NobodyCanSeeME#075488\", \"authority\":\"user\" }"))
+    protected override def bodyToStick: Option[String] = Some(new String("{\"email\":\"sandy@megamsandbox.com\", \"api_key\":\"IamAtlas{74}NobodyCanSeeME#07\", \"authority\":\"user\" }"))
     protected def headersOpt: Option[Map[String, String]] = None
 
     private val post = POST(url)(httpClient)
@@ -67,9 +68,11 @@ class AuthenticateSpec extends Specification {
 
   case object PostWithInvalidUserIDEmail extends Context {
     protected override def urlSuffix: String = "auth"
-    protected override def bodyToStick: Option[String] = Some(new String("{\"email\":\"sandy123@megamsandbox.com\", \"api_key\":\"IamAtlas{74}NobodyCanSeeME#075488\", \"authority\":\"user\" }"))
-
-    protected def headersOpt: Option[Map[String, String]] = None
+    protected override def bodyToStick: Option[String] = Some(new String("{\"email\":\"sand2y123@megamsandbox.com\", \"api_key\":\"IamAtlas{74}NobodyCanSeeME#07\", \"authority\":\"user\" }"))
+     protected override def headersOpt: Option[Map[String, String]] = Some(Map(Content_Type -> application_json,
+     X_Megam_EMAIL -> "sand2y123@megamsandbox.com", X_Megam_APIKEY-> "IamAtlas{74}NobodyCanSeeME#07",
+     X_Megam_DATE -> currentDate, Accept -> application_vnd_megam_json))
+ 
 
     private val post = POST(url)(httpClient)
       .addHeaders(headers)
@@ -83,10 +86,10 @@ class AuthenticateSpec extends Specification {
 
   case object PostMalformedHeader extends Context {
     protected override def urlSuffix: String = "auth"
-    protected override def bodyToStick: Option[String] = Some(new String("{\"email\":\"sandy@megamsandbox.com\", \"api_key\":\"IamAtlas{74}NobodyCanSeeME#075488\", \"authority\":\"user\" }"))
+    protected override def bodyToStick: Option[String] = Some(new String("{\"email\":\"sandy@megamsandbox.com\", \"api_key\":\"IamAtlas{74}NobodyCanSeeME#07\", \"authority\":\"user\" }"))
 
-    protected override def headersOpt: Option[Map[String, String]] = some(Map(Content_Type -> application_json,
-    X_Megam_EMAIL123 -> "sandy@megamsandbox.com", X_Megam_APIKEY_458-> "IamAtlas{74}NobodyCanSeeME#07",
+    protected override def headersOpt: Option[Map[String, String]] = Some(Map(Content_Type -> application_json,
+    X_Megam_EMAIL -> "sandy555@megaamsandbox.com", X_Megam_APIKEY-> "IamAtllidkfas{74}NobodyCanSeeME#07",
     X_Megam_DATE -> currentDate, Accept -> application_vnd_megam_json))
 
     private val post = POST(url)(httpClient)
