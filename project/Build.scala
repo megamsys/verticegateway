6984c4850673d4a4a810f542c4ffedff3ee23410
Kishorekumar Neelamegam <nkishore@megam.co.in>
2013-09-05 11:40:25 +0530

    Added doc link in left menu, removed transitive dependencies from play's build.scala.

--- project/Build.scala
+++ project/Build.scala
@@ -25,29 +25,19 @@ object ApplicationBuild extends Build {
   val scalaCheckVersion = "1.10.1"
   val play2AuthVersion = "0.10.1"
   val specs2Version = "2.1.1"
-  val jodaTimeVersion = "0.4.2"
   val liftJsonVersion = "2.5.1"
   val megamVersion = "0.1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    javaCore, javaEbean, 
-    "org.scalaz" %% "scalaz-core" % scalazVersion,
-    "org.scalaz" %% "scalaz-iteratee" % scalazVersion,
-    "org.scalaz" %% "scalaz-effect" % scalazVersion,
-    "org.scalaz" %% "scalaz-iterv" % scalazVersion,
-    "org.scalaz" %% "scalaz-typelevel" % scalazVersion,
-    "net.liftweb" %% "lift-json-scalaz7" % liftJsonVersion,
-    "com.github.indykish" % "megam_common_2.10" % megamVersion,
+    javaCore, javaEbean,
+    "com.twitter.service" % "snowflake" % "1.0.2" from "https://s3-ap-southeast-1.amazonaws.com/megampub/0.1/jars/snowflake.jar", //don't move this below.
+    "com.github.indykish" % "megam_common_2.10" % megamVersion excludeAll (
+      ExclusionRule("commons-logging","commons-logging"),
+      ExclusionRule("org.slf4j","slf4j-jdk14")),
     "com.github.mumoshu" %% "play2-memcached" % "0.3.0.2",
     "jp.t2v" %% "play2.auth" % play2AuthVersion,
     "jp.t2v" %% "play2.auth.test" % play2AuthVersion % "test",
-    "com.rabbitmq" % "amqp-client" % "3.1.4",
-    "com.github.nscala-time" %% "nscala-time" % jodaTimeVersion,
-    "org.scalacheck" %% "scalacheck" % scalaCheckVersion % "test",
-    "org.specs2" %% "specs2" % specs2Version % "test",
-    "com.stackmob" %% "newman" % "0.23.0" % "test",    
-    "com.twitter.service" % "snowflake" % "1.0.2" from "https://s3-ap-southeast-1.amazonaws.com/megampub/0.1/jars/snowflake.jar" //don't move this below.
-    )
+    "com.stackmob" %% "newman" % "0.23.0" % "test")
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     sbt.Keys.resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
