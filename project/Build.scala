951eecf288503399f884f16fdf8d228dbba25753
Kishorekumar Neelamegam <nkishore@megam.co.in>
2013-11-04 18:42:43 +0530

    upgrade play to 2.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -33,7 +33,7 @@ object ApplicationBuild extends Build {
     "com.github.mumoshu" %% "play2-memcached" % "0.3.0.2",
     "jp.t2v" %% "play2-auth" % play2AuthVersion,
     "jp.t2v" %% "play2-auth-test" % play2AuthVersion % "test",
-    "com.stackmob" %% "newman" % "1.3.0" % "test")
+    "com.stackmob" %% "newman" % "1.3.1" % "test")
 
   val main = play.Project(appName, appVersion, appDependencies).settings(    
     sbt.Keys.resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
