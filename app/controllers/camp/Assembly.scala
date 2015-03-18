ce711c38a4b25ded4e2909a7cf0e9888b5503ae6
Rajthilak <rajthilak@megam.io>
2015-03-18 15:18:39 +0530

    test

--- app/controllers/camp/Assembly.scala
+++ app/controllers/camp/Assembly.scala
@@ -130,6 +130,27 @@ object Assembly extends Controller with APIAuthElement {
     }).fold(succ = { a: Result => a }, fail = { t: Throwable => Status(BAD_REQUEST)(t.getMessage) })
   }*/
 
+ def build(id: String, cid: String) = Action(parse.tolerantText) { implicit request =>
+    play.api.Logger.debug(("%-20s -->[%s]").format("controllers.Assembly", "show:Entry"))
+    play.api.Logger.debug(("%-20s -->[%s]").format("Assembly ID", id))
+    play.api.Logger.debug(("%-20s -->[%s]").format("Component ID", cid))
+
+    (Validation.fromTryCatch[Result] {     
+         ContiniousIntegrationNotifyPublish(id, cid, "notify").dop.flatMap { x =>
+                play.api.Logger.debug(("%-20s -->[%s]").format("controllers.CINotify", "published successfully."))
+                Status(CREATED)(FunnelResponse(CREATED, """CI notify initiation instruction submitted successfully.
+            |
+            |The Assembly build request is working for you. It will be ready shortly.""", "Megam::Assembly").toJson(true)).successNel[Throwable]
+              } match {
+                //this is only a temporary hack.
+                case Success(succ_cpc) => succ_cpc
+                case Failure(err) =>
+                  Status(BAD_REQUEST)(FunnelResponse(BAD_REQUEST, """CI notify initiation submission failed.
+            |
+            |Retry again, our queue servers are crowded""", "Megam::Assembly").toJson(true))
+              }            
+    }).fold(succ = { a: Result => a }, fail = { t: Throwable => Status(BAD_REQUEST)(t.getMessage) })
+ }
  
 
 }
