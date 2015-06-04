040f33f64d9a32259f615ce65708216c13fac213
Yeshwanth kumar <getyesh@megam.io>
2015-06-04 18:48:28 +0530

    tosca type addition

--- app/models/tosca/CSARJson.scala
+++ app/models/tosca/CSARJson.scala
@@ -247,17 +247,24 @@ object CSARJson {
             }
           }
           if (flag != true) {
-            assemblylist += new Assembly(getRandomName(), List(cvalue), "", KeyValueList.empty, PoliciesList.empty, KeyValueList.empty, OperationList.empty, KeyValueList.empty, "")
+            
+            assemblylist += new Assembly(getRandomName(), List(cvalue), value.tosca_type, KeyValueList.empty, PoliciesList.empty, KeyValueList.empty, OperationList.empty, KeyValueList.empty, "")
           }
       }
     } else {
       componentList foreach {
         case (cvalue) =>
-          assemblylist += new Assembly(getRandomName(), List(cvalue), "", KeyValueList.empty, PoliciesList.empty, KeyValueList.empty, OperationList.empty, KeyValueList.empty, "")
+          assemblylist += new Assembly(getRandomName(), List(cvalue), cvalue.tosca_type , KeyValueList.empty, PoliciesList.empty, KeyValueList.empty, OperationList.empty, KeyValueList.empty, "")
       }
     }
     return assemblylist
   }
+  
+ // def toscaType(tosca_type: Unit): String = {
+ //    println(tosca_type)
+ // }
+ // 
+  
 
   def assembliesBuilder(assemblyList: scala.collection.mutable.MutableList[Assembly]): String = {
     var assembly_lists = new ListBuffer[Assembly]()
