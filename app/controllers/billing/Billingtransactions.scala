12ee23ef9aff328bfecd0a1f12aaf9046f6c8f99
Kishorekumar Neelamegam <nkishore@megam.co.in>
2016-10-11 13:40:06 +0530

    Fix for #420 #418

--- app/controllers/billing/Billingtransactions.scala
+++ app/controllers/billing/Billingtransactions.scala
@@ -2,38 +2,46 @@ package controllers.billing
 
 import scalaz._
 import Scalaz._
+import scalaz.Validation
+import scalaz.Validation.FlatMap._
 import scalaz.NonEmptyList._
-import scalaz.Validation._
-
-import io.megam.auth.funnel._
-import io.megam.auth.funnel.FunnelErrors._
-import models.billing._
-import play.api.mvc._
-import controllers.stack.Results
 import net.liftweb.json._
 import net.liftweb.json.JsonParser._
+
+import io.megam.auth.funnel.{ FunnelResponse, FunnelResponses }
+import io.megam.auth.funnel.FunnelErrors._
+import io.megam.auth.stack.Role._
+
+import play.api.mvc._
+import models.billing._
+import controllers.stack.Results
+import controllers.stack.{APIAuthElement, PermissionElement}
+
 /**
  * @author rajesh
  *
  */
 
-object Billingtransactions extends Controller with controllers.stack.APIAuthElement {
+object Billingtransactions extends Controller with APIAuthElement with PermissionElement {
 implicit val formats = DefaultFormats
   /**
    * Create a new billing transcations for the user.
    **/
 
-  def post = StackAction(parse.tolerantText) {  implicit request =>
+  def post = StackAction(parse.tolerantText, AuthorityKey -> Administrator) {  implicit request =>
     (Validation.fromTryCatchThrowable[Result,Throwable] {
       reqFunneled match {
         case Success(succ) => {
-          val freq = succ.getOrElse(throw new Error("Invalid header."))
-          val email = freq.maybeEmail.getOrElse(throw new Error("Email not found (or) invalid."))
+          val freq   = succ.getOrElse(throw new CannotAuthenticateError("Invalid header.", "Read docs.megam.io/api."))
+          val email  = freq.maybeEmail.getOrElse(throw new CannotAuthenticateError("Email not found (or) invalid.", "Read docs.megam.io/api."))
+          val org    = freq.maybeOrg.getOrElse(throw new CannotAuthenticateError("Org not found (or) invalid.", "Read docs.megam.io/api."))
+          val admin  = canPermit(grabAuthBag).getOrElse(throw new PermissionNotThere("admin authority is required to access this resource.", "Read docs.megam.io/api."))
           val clientAPIBody = freq.clientAPIBody.getOrElse(throw new Error("Body not found (or) invalid."))
+
           models.billing.Billingtransactions.create(email, clientAPIBody) match {
             case Success(succ) =>
               Status(CREATED)(
-                FunnelResponse(CREATED, "Billingtransactions record created successfully."  , "Megam::BillingTransactions").toJson(true))
+                FunnelResponse(CREATED, "Billingtransaction created."  , "Megam::BillingTransactions").toJson(true))
             case Failure(err) =>
               val rn: FunnelResponse = new HttpReturningError(err)
               Status(rn.code)(rn.toJson(true))
@@ -44,7 +52,7 @@ implicit val formats = DefaultFormats
           Status(rn.code)(rn.toJson(true))
         }
       }
-    }).fold(succ = { a: Result => a }, fail = { t: Throwable => Status(BAD_REQUEST)(t.getMessage) })
+    }).fold(succ = { a: Result => a }, fail = { t: Throwable =>   { val rn: FunnelResponse = new HttpReturningError(nels(t));  Status(rn.code)(rn.toJson(true)) } })
    }
 
  /**
