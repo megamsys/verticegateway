4c511a9dfb258352de94122f1ac485a47a0a19cf
Kishorekumar Neelamegam <nkishore@megam.co.in>
2016-12-28 20:18:34 +0530

    Disk fix

--- app/controllers/disks/Disks.scala
+++ app/controllers/disks/Disks.scala
@@ -9,7 +9,7 @@ import io.megam.auth.funnel._
 import io.megam.auth.funnel.FunnelErrors._
 import play.api.mvc._
 import controllers.stack.Results
-
+import models.Constants._
 
 object Disks extends Controller with controllers.stack.APIAuthElement {
 
@@ -23,7 +23,7 @@ def post = StackAction(parse.tolerantText) { implicit request =>
         models.disks.Disks.create(email, clientAPIBody) match {
           case Success(succ) =>
             Status(CREATED)(
-              FunnelResponse(CREATED, """Disk created successfully.""", "Megam::Disks").toJson(true))
+              FunnelResponse(CREATED, """Disk attached successfully.""", DISKSCLAZ).toJson(true))
           case Failure(err) =>
             val rn: FunnelResponse = new HttpReturningError(err)
             Status(rn.code)(rn.toJson(true))
@@ -43,7 +43,7 @@ def post = StackAction(parse.tolerantText) { implicit request =>
         case Success(succ) => {
           val freq = succ.getOrElse(throw new Error("Invalid header."))
           val email = freq.maybeEmail.getOrElse(throw new Error("Email not found (or) invalid."))
-          models.disks.Disks.findById(id,email) match {
+          models.disks.Disks.findByAssemblyId(id,email) match {
             case Success(succ) => Ok(Results.resultset(models.Constants.DISKSCOLLECTIONCLAZ, compactRender(Extraction.decompose(succ))))
             case Failure(err) =>
               val rn: FunnelResponse = new HttpReturningError(err)
@@ -65,7 +65,7 @@ def post = StackAction(parse.tolerantText) { implicit request =>
         case Success(succ) => {
           val freq = succ.getOrElse(throw new Error("Invalid header."))
           val email = freq.maybeEmail.getOrElse(throw new Error("Email not found (or) invalid."))
-          models.disks.Disks.getById(id,email) match {
+          models.disks.Disks.findById(id,email) match {
             case Success(succ) => Ok(Results.resultset(models.Constants.DISKSCOLLECTIONCLAZ, compactRender(Extraction.decompose(succ))))
             case Failure(err) =>
               val rn: FunnelResponse = new HttpReturningError(err)
@@ -113,7 +113,7 @@ def post = StackAction(parse.tolerantText) { implicit request =>
           models.disks.Disks.update(email, clientAPIBody) match {
             case Success(succ) =>
               Status(CREATED)(
-                FunnelResponse(CREATED, "Your Disks updated successfully.", "Megam::Disks").toJson(true))
+                FunnelResponse(CREATED, "Your Disks updated successfully.", DISKSCLAZ).toJson(true))
             case Failure(err) =>
               val rn: FunnelResponse = new HttpReturningError(err)
               Status(rn.code)(rn.toJson(true))
@@ -126,6 +126,7 @@ def post = StackAction(parse.tolerantText) { implicit request =>
       }
     }).fold(succ = { a: Result => a }, fail = { t: Throwable => Status(BAD_REQUEST)(t.getMessage) })
   }
+
   def delete(asm_id: String, id: String) = StackAction(parse.tolerantText) { implicit request =>
     (Validation.fromTryCatchThrowable[Result, Throwable] {
       reqFunneled match {
@@ -135,7 +136,30 @@ def post = StackAction(parse.tolerantText) { implicit request =>
           models.disks.Disks.delete(email, asm_id, id) match {
             case Success(succ) =>
               Status(CREATED)(
-                FunnelResponse(CREATED, "Disk deleted successfully.", "Megam::Disk").toJson(true))
+                FunnelResponse(CREATED, "Disk removed successfully.", DISKSCLAZ).toJson(true))
+            case Failure(err) =>
+              val rn: FunnelResponse = new HttpReturningError(err)
+              Status(rn.code)(rn.toJson(true))
+          }
+        }
+        case Failure(err) => {
+          val rn: FunnelResponse = new HttpReturningError(err)
+          Status(rn.code)(rn.toJson(true))
+        }
+      }
+    }).fold(succ = { a: Result => a }, fail = { t: Throwable => Status(BAD_REQUEST)(t.getMessage) })
+  }
+
+  def initdel(asm_id: String, id: String) = StackAction(parse.tolerantText) { implicit request =>
+    (Validation.fromTryCatchThrowable[Result, Throwable] {
+      reqFunneled match {
+        case Success(succ) => {
+          val freq = succ.getOrElse(throw new Error("Invalid header."))
+          val email = freq.maybeEmail.getOrElse(throw new Error("Email not found (or) invalid."))
+          models.disks.Disks.initdel(email, asm_id, id) match {
+            case Success(succ) =>
+              Status(CREATED)(
+                FunnelResponse(CREATED, "Disk removal started.", DISKSCLAZ).toJson(true))
             case Failure(err) =>
               val rn: FunnelResponse = new HttpReturningError(err)
               Status(rn.code)(rn.toJson(true))
